4856f054bbecf0ffb50319afda02738c2877b839
KeerthanaPulivarthi <sowbhikeerthi@gmail.com>
2025-04-23 14:40:42 -0500

    Update day1.sql

--- Week1/day1/day1.sql
+++ Week1/day1/day1.sql
@@ -1,13 +1,13 @@
-🧾 2. `day01_fitness_tracker.sql`
+--You can export or manually write your SQL commands in a `.sql` file:
 
-You can export or manually write your SQL commands in a `.sql` file:
+--Create and select database
 
-```sql
 USE my_pratice; #create a database mypratice.
 
 show databases; # shows list of databases.
 
-#created a table 
+--created a table :
+
 CREATE TABLE fitness_tracker (
     user_id INT,
     workout_date DATE,
@@ -16,7 +16,8 @@ CREATE TABLE fitness_tracker (
     calories_burned INT
 );
 
-# insert values into table:
+-- insert values into table:
+
 INSERT INTO fitness_tracker (user_id, workout_date, workout_type, workout_time, calories_burned)
 VALUES 
 (101, '2025-04-02', 'cardio', 30, 157),
@@ -26,5 +27,6 @@ VALUES
 (105, '2025-04-02', 'treadmill', 30, 181),
 (106, '2025-04-02', 'cycling', 30, 161);
 
-#select the data from a database:
+--select the data from a database:
+
 SELECT * FROM fitness_tracker;
